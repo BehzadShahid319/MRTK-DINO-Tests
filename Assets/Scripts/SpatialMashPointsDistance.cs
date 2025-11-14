509c5edd1fb042ba2bb73d7817d59d1cbd1eeda4
BehzadShahid319 <behzadshahid95@gmail.com>
2025-11-15 07:46:01 +0800

    Minor Changes

--- Assets/Scripts/SpatialMashPointsDistance.cs
+++ Assets/Scripts/SpatialMashPointsDistance.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class SpatialMashPointsDistance : MonoBehaviour
 {
     [SerializeField] Transform trackedObject;
+    [SerializeField] MeshRenderer trackedObjectMesh;
     [SerializeField] Vector3 tipOffset;
     [SerializeField] float rayLength;
 
+    [SerializeField] Material defaultMaterial;
+    [SerializeField] Material triggeredMaterial;
+
     Vector3 tipPosition;
+    Vector3 rayTargetPoint;
+    private void Update()
+    {
+        tipPosition = trackedObject.position + trackedObject.rotation * tipOffset; //Get proper offset if object is rotated
+        rayTargetPoint = -(rayLength * trackedObject.up) + tipPosition;
+        detectTipCollision();
+    }
 
     private void OnDrawGizmosSelected()
     {
@@ -16,5 +27,17 @@ public class SpatialMashPointsDistance : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawLine(tipPosition, -(rayLength * trackedObject.up) + tipPosition);
         //Gizmos.DrawLine(trackedObject.position, -(rayLength * trackedObject.up)+trackedObject.position);
+    }
+
+    void detectTipCollision()
+    {
+        if (Physics.Raycast(tipPosition, rayTargetPoint, out RaycastHit hit, rayLength, LayerMask.GetMask("Spatial Awareness")))
+        {
+            trackedObjectMesh.material = triggeredMaterial;
+        }
+        else
+        {
+            trackedObjectMesh.material = defaultMaterial;
+        }
     }
 }
