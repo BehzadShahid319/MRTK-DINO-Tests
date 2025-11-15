caf36de7c27a8456f2292a9b7b9ffdd8e7af8c09
BehzadShahid319 <behzadshahid95@gmail.com>
2025-11-15 10:34:16 +0800

    Raycast Hit with Spatial Awareness Issue Fixed

--- Assets/Scripts/SpatialMashPointsDistance.cs
+++ Assets/Scripts/SpatialMashPointsDistance.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpatialMashPointsDistance : MonoBehaviour
 {
+    [SerializeField] LineRenderer lineRendererForDisplay;
     [SerializeField] Transform trackedObject;
     [SerializeField] MeshRenderer trackedObjectMesh;
     [SerializeField] Vector3 tipOffset;
@@ -14,30 +15,48 @@ public class SpatialMashPointsDistance : MonoBehaviour
 
     Vector3 tipPosition;
     Vector3 rayTargetPoint;
+    Vector3 direction;
+
+    private void Start()
+    {
+        lineRendererForDisplay.useWorldSpace = true;
+    }
+
     private void Update()
     {
+        direction = -trackedObject.up;
         tipPosition = trackedObject.position + trackedObject.rotation * tipOffset; //Get proper offset if object is rotated
-        rayTargetPoint = -(rayLength * trackedObject.up) + tipPosition;
-        detectTipCollision();
+        //rayTargetPoint = -(rayLength * trackedObject.up) + tipPosition;
+        rayTargetPoint = tipPosition + direction * rayLength;
+
+        // Ray direction: stylus "down" (or forward, depending on your model)
+        Vector3 rayEnd = tipPosition - trackedObject.up * rayLength;
+
+        lineRendererForDisplay .SetPosition(0, tipPosition);
+        lineRendererForDisplay.SetPosition(1, rayTargetPoint);
+
+        detectTipCollision(direction);
     }
 
     private void OnDrawGizmosSelected()
     {
         tipPosition = trackedObject.position + trackedObject.rotation * tipOffset; //Get proper offset if object is rotated
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(tipPosition, -(rayLength * trackedObject.up) + tipPosition);
+        Gizmos.DrawLine(tipPosition, tipPosition - trackedObject.up * rayLength);
         //Gizmos.DrawLine(trackedObject.position, -(rayLength * trackedObject.up)+trackedObject.position);
     }
 
-    void detectTipCollision()
+    void detectTipCollision(Vector3 dir)
     {
-        if (Physics.Raycast(tipPosition, rayTargetPoint, out RaycastHit hit, rayLength, LayerMask.GetMask("Spatial Awareness")))
+        if (Physics.Raycast(tipPosition, dir, out RaycastHit hit, rayLength, LayerMask.GetMask("Spatial Awareness")))
         {
             trackedObjectMesh.material = triggeredMaterial;
+            Debug.Log("<color=yellow> Triggerred with Spatial Awareness Mesh</color>");
         }
         else
         {
             trackedObjectMesh.material = defaultMaterial;
+            Debug.Log("<color=red> Triggerred Exit From SPatial Awareness Mesh</color>");
         }
     }
 }
